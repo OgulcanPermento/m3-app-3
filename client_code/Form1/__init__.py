6ffafdfa850d66a61d9c23edc7da0d3d8bb2a20b
OgulcanPermento <ogulcan@permentostudio.nl>
2025-06-17 13:39:16 +0200

    Added professional dashboard layout with modern styling and interactive features

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -9,22 +9,29 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Get text from server and set it to the label
-    self.text_label.text = anvil.server.call('get_text')
+    server_text = anvil.server.call('get_text')
+    self.text_label.text = server_text
     
     # Update the footer with current year
     current_year = datetime.now().year
-    self.footer_label.text = f'© {current_year} My App'
+    self.footer_label.text = f'© {current_year} Professional Dashboard'
     
-    # Add some hover effects
-    self.main_content.set_event_handler('mouse_enter', self.on_main_content_hover)
-    self.main_content.set_event_handler('mouse_leave', self.on_main_content_leave)
+    # Add hover effects to panels
+    self.stats_panel.set_event_handler('mouse_enter', self.on_panel_hover)
+    self.stats_panel.set_event_handler('mouse_leave', self.on_panel_leave)
+    self.info_panel.set_event_handler('mouse_enter', self.on_panel_hover)
+    self.info_panel.set_event_handler('mouse_leave', self.on_panel_leave)
     
-  def on_main_content_hover(self, **event_args):
-    """This method is called when the mouse enters the main content area"""
-    self.main_content.background = '#e8eaf6'  # Lighter shade of the background
+  def on_panel_hover(self, **event_args):
+    """This method is called when the mouse enters a panel"""
+    panel = event_args['sender']
+    panel.background = '#f5f5f5'
+    panel.border = '1px solid #1a237e'
     
-  def on_main_content_leave(self, **event_args):
-    """This method is called when the mouse leaves the main content area"""
-    self.main_content.background = '#f5f6fa'  # Original background color
+  def on_panel_leave(self, **event_args):
+    """This method is called when the mouse leaves a panel"""
+    panel = event_args['sender']
+    panel.background = 'white'
+    panel.border = '1px solid #e0e0e0'
 
     # Any code you write here will run before the form opens.
